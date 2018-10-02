582671559616a40483d82b8406e679a3afa3372d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-02 10:05:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,20 @@ class App < Sinatra::Base
 		erb :index
 	end
 	
-	get "/info" do
+	get '/info' do
       erb :info
     end
 end
+
+# require_relative 'config/environment'
+
+# class App < Sinatra::Base
+
+# 	get '/' do
+# 		erb :index  
+# 	end
+
+#   get '/info' do 
+#     erb :info
+#   end
+# end
